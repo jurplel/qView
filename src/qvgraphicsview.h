45d175f62a45d37cda2107fa957fbaae59b0c3d8
Benjamin O <jeep70cp@gmail.com>
2018-09-01 00:17:17 -0400

    resolved #144

--- src/qvgraphicsview.h
+++ src/qvgraphicsview.h
@@ -93,7 +93,7 @@ protected:
 
     void mouseDoubleClickEvent(QMouseEvent *event) override;
 
-    void fitInViewMarginless();
+    void fitInViewMarginless(bool setVariables = true);
 
 
 private slots:
