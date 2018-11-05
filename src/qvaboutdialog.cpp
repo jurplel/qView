f73bf29eb534a7952cf67c2aad9a40b67a2a4d81
Benjamin O <jeep70cp@gmail.com>
2018-11-04 19:41:11 -0500

    updated github urls

--- src/qvaboutdialog.cpp
+++ src/qvaboutdialog.cpp
@@ -43,7 +43,7 @@ QVAboutDialog::QVAboutDialog(QWidget *parent) :
     //set infolabel2 font, text, and properties
     QFont font4 = QFont("Lato", 8 + modifier);
     font4.setStyleName("Regular");
-    const QString labelText2 = tr("Built with Qt %1<br>Source code available under GPLv3 at <a style=\"color: #03A9F4; text-decoration:none;\" href=\"https://github.com/jeep70/qView\">Github</a><br>Icon glyph created by Guilhem from the Noun Project<br>Copyright © 2018, jurplel and qView contributors").arg(QT_VERSION_STR);
+    const QString labelText2 = tr("Built with Qt %1<br>Source code available under GPLv3 at <a style=\"color: #03A9F4; text-decoration:none;\" href=\"https://github.com/jurplel/qView\">Github</a><br>Icon glyph created by Guilhem from the Noun Project<br>Copyright © 2018, jurplel and qView contributors").arg(QT_VERSION_STR);
     ui->infoLabel2->setFont(font4);
     ui->infoLabel2->setText(labelText2);
 
@@ -60,7 +60,7 @@ QVAboutDialog::~QVAboutDialog()
 void QVAboutDialog::requestUpdates()
 {
     // send network request for update check
-    QUrl url("https://api.github.com/repos/jeep70/qview/releases");
+    QUrl url("https://api.github.com/repos/jurplel/qview/releases");
     QNetworkRequest request(url);
     request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
 
@@ -96,7 +96,7 @@ void QVAboutDialog::checkUpdates(QNetworkReply* reply)
     }
     if (latestVersionNum > VERSION)
     {
-        const QString text = tr("<a style=\"color: #03A9F4; text-decoration:none;\" href=\"https://github.com/jeep70/qView/releases\">%1 update available!</a>").arg(QString::number(latestVersionNum, 'f', 1));
+        const QString text = tr("<a style=\"color: #03A9F4; text-decoration:none;\" href=\"https://github.com/jurplel/qView/releases\">%1 update available!</a>").arg(QString::number(latestVersionNum, 'f', 1));
         ui->updateLabel->setText(text);
         ui->updateLabel->setTextInteractionFlags(Qt::TextBrowserInteraction);
         ui->updateLabel->setOpenExternalLinks(true);
