c950862a4f2897f9a77b4c8cf419e73223acc437
Benjamin O <jeep70cp@gmail.com>
2019-03-23 19:00:51 -0400

    Fixed Qt 5.9 compatibility

--- src/qvinfodialog.h
+++ src/qvinfodialog.h
@@ -3,6 +3,7 @@
 
 #include <QDialog>
 #include <QFileInfo>
+#include <QLocale>
 
 namespace Ui {
 class QVInfoDialog;
@@ -28,6 +29,53 @@ private:
     int height;
 
     int frameCount;
+
+public:
+    static QString formatBytes(qint64 bytes)
+    {
+        QString sizeString;
+        #if (QT_VERSION >= QT_VERSION_CHECK(5, 10, 0))
+        QLocale locale;
+        sizeString = locale.formattedDataSize(bytes);
+        #else
+
+        double size = bytes;
+
+        int reductionAmount = 0;
+        double newSize = size/1024;
+        while (newSize > 1024)
+        {
+            newSize /= 1024;
+            reductionAmount++;
+            if (reductionAmount > 2)
+                break;
+        }
+
+        QString unit;
+        switch (reductionAmount)
+        {
+        case 0: {
+            unit = " KiB";
+            break;
+        }
+        case 1: {
+            unit = " MiB";
+            break;
+        }
+        case 2: {
+            unit = " GiB";
+            break;
+        }
+        case 3: {
+            unit = " TiB";
+            break;
+        }
+        }
+
+        sizeString = QString::number(newSize, 'f', 2) + unit;
+        #endif
+        return sizeString;
+    }
 };
 
 #endif // QVINFODIALOG_H
