bad803688df235ee26fd918711069d57e28b6038
Benjamin O <jeep70cp@gmail.com>
2019-04-17 14:33:19 -0400

    Changed about dialog wording slightly

--- src/qvaboutdialog.cpp
+++ src/qvaboutdialog.cpp
@@ -43,7 +43,7 @@ QVAboutDialog::QVAboutDialog(QWidget *parent) :
     //set infolabel2 font, text, and properties
     QFont font4 = QFont("Lato", 8 + modifier);
     font4.setStyleName("Regular");
-    const QString labelText2 = tr("Built with Qt %1<br>Source code available under GPLv3 at <a style=\"color: #03A9F4; text-decoration:none;\" href=\"https://github.com/jurplel/qView\">Github</a><br>Icon glyph created by Guilhem from the Noun Project<br>Copyright © 2018-2019, jurplel and qView contributors").arg(QT_VERSION_STR);
+    const QString labelText2 = tr("Built with Qt %1<br>Source code available under GPLv3 on <a style=\"color: #03A9F4; text-decoration:none;\" href=\"https://github.com/jurplel/qView\">Github</a><br>Icon glyph created by Guilhem from the Noun Project<br>Copyright © 2018-2019 jurplel and qView contributors").arg(QT_VERSION_STR);
     ui->infoLabel2->setFont(font4);
     ui->infoLabel2->setText(labelText2);
 
