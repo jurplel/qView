b4a7cc206bbb526c806a361618c9ad75389e3d5a
Benjamin O <jeep70cp@gmail.com>
2019-07-16 23:15:55 -0400

    simplified shortcut modification dialog

--- src/qvshortcutdialog.cpp
+++ src/qvshortcutdialog.cpp
@@ -1,6 +1,8 @@
 #include "qvshortcutdialog.h"
 #include "ui_qvshortcutdialog.h"
 
+#include <QDebug>
+
 QVShortcutDialog::QVShortcutDialog(const SShortcut &shortcut_object, int i, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::QVShortcutDialog)
@@ -12,11 +14,7 @@ QVShortcutDialog::QVShortcutDialog(const SShortcut &shortcut_object, int i, QWid
 
     shortcut_obj = shortcut_object;
     index = i;
-
-    foreach (QString shortcut, shortcut_obj.shortcuts)
-    {
-        ui->shortcutsList->addItem(shortcut);
-    }
+    ui->keySequenceEdit->setKeySequence(shortcut_obj.shortcuts.join(", "));
 }
 
 QVShortcutDialog::~QVShortcutDialog()
@@ -24,47 +22,17 @@ QVShortcutDialog::~QVShortcutDialog()
     delete ui;
 }
 
-void QVShortcutDialog::on_addButton_clicked()
-{
-    if (!ui->keySequenceEdit->keySequence().isEmpty())
-        ui->shortcutsList->addItem(ui->keySequenceEdit->keySequence().toString());
-}
-
-void QVShortcutDialog::on_subtractButton_clicked()
-{
-    foreach (QListWidgetItem *item, ui->shortcutsList->selectedItems())
-    {
-        ui->shortcutsList->takeItem(ui->shortcutsList->row(item));
-    }
-}
-
-void QVShortcutDialog::on_keySequenceEdit_editingFinished()
-{
-    int value = ui->keySequenceEdit->keySequence()[0];
-    QKeySequence shortcut(value);
-    ui->keySequenceEdit->setKeySequence(shortcut);
-}
-
 void QVShortcutDialog::on_buttonBox_clicked(QAbstractButton *button)
 {
     if (ui->buttonBox->buttonRole(button) == QDialogButtonBox::AcceptRole)
     {
-        QStringList newShortcutsList;
-        for(int i = 0; i < ui->shortcutsList->count(); i++)
-        {
-            QListWidgetItem* item = ui->shortcutsList->item(i);
-            newShortcutsList << item->text();
-        }
+        QStringList newShortcutsList = ui->keySequenceEdit->keySequence().toString().split(", ");
         shortcut_obj.shortcuts = newShortcutsList;
 
         emit newShortcut(shortcut_obj, index);
     }
     else if (ui->buttonBox->buttonRole(button) == QDialogButtonBox::ResetRole)
     {
-        ui->shortcutsList->clear();
-        foreach (QString shortcut, shortcut_obj.defaultShortcuts)
-        {
-            ui->shortcutsList->addItem(shortcut);
-        }
+        ui->keySequenceEdit->setKeySequence(shortcut_obj.defaultShortcuts.join(", "));
     }
 }
