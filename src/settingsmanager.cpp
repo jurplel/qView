ac52fab7eb4d0f70dc04bc8c2a8dcb34c4b41720
jurplel <jeep70cp@gmail.com>
2020-11-04 16:32:52 -0500

    make an english base file as well

--- src/settingsmanager.cpp
+++ src/settingsmanager.cpp
@@ -17,8 +17,7 @@ SettingsManager::SettingsManager(QObject *parent) : QObject(parent)
 bool SettingsManager::loadTranslation()
 {
     QTranslator *translator = new QTranslator();
-//    bool success = translator->load(QLocale::system(), QLatin1String("qview"), QLatin1String("_"), QLatin1String(":/i18n"));
-    bool success = translator->load(":/i18n/qview_test.ts");
+    bool success = translator->load(QLocale::system(), QLatin1String("qview"), QLatin1String("_"), QLatin1String(":/i18n"));
     if (success)
     {
         qInfo() << "Loaded translation";
