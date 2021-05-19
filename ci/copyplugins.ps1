cb185d3045e57c581c02b5a0e4b49675663006eb
Benjamin O <jeep70cp@gmail.com>
2021-05-19 19:59:58 -0400

    try to fix plugins and test with plugins/non-nightly mode

--- ci/copyplugins.ps1
+++ ci/copyplugins.ps1
@@ -2,13 +2,13 @@
 
 try {
     if ($IsWindows) {
-        copy ..\QtApng\plugins\imageformats\qapng.dll bin\imageformats\
+        copy QtApng\plugins\imageformats\qapng.dll bin\imageformats\
 
-        copy ..\libde265\libde265\libde265.dll bin\
-        copy ..\libde265\libheif\libheif\heif.dll bin\
-        copy ..\libde265\libheif\qt-heif-image-plugin\bin\imageformats\qheif.dll bin\imageformats\
+        copy libde265\libde265\libde265.dll bin\
+        copy libde265\libheif\libheif\heif.dll bin\
+        copy libde265\libheif\qt-heif-image-plugin\bin\imageformats\qheif.dll bin\imageformats\
         
-        copy ..\qt-avif-image-plugin\plugins\imageformats\qavif.dll bin\imageformats\
+        copy qt-avif-image-plugin\plugins\imageformats\qavif.dll bin\imageformats\
     } elseif ($IsMacOS) {
         cp $env:Qt5_DIR/plugins/imageformats/libqapng.dylib bin/qView.app/Contents/PlugIns/imageformats/
         cp $env:Qt5_DIR/plugins/imageformats/libqavif.dylib bin/qView.app/Contents/PlugIns/imageformats/
