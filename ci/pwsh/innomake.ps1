a3a24983e90aa28e67d813f953a72f79be2c24c8
Benjamin O <jeep70cp@gmail.com>
2021-05-19 22:59:39 -0400

    fix innomake for real

--- ci/pwsh/innomake.ps1
+++ ci/pwsh/innomake.ps1
@@ -1,5 +1,6 @@
 # Script assumes $env:arch will start with win64 or win32
-mkdir ..\dist\win\qView-$env:arch.substring(0, 5)
-copy -R bin\* ..\dist\win\qView-$env:arch.substring(0, 5)
-iscc ..\dist\win\qView$($env:arch.substring(3, 2)).iss
-copy ..\dist\win\Output\* .\
+$win64orwin32 = $env:arch.substring(0, 5)
+New-Item -Path "dist\win\qView-$win64orwin32" -ItemType Directory -ea 0
+copy -R bin\* "dist\win\qView-$win64orwin32"
+iscc dist\win\qView$($env:arch.substring(3, 2)).iss
+copy dist\win\Output\* bin\
