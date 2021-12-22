46db245430bcf25c090249a24e468af983f73ccb
jurplel <jeep70cp@gmail.com>
2021-12-22 01:13:37 -0500

    Fix #432 Wayland generic icon on KDE

--- src/qvapplication.cpp
+++ src/qvapplication.cpp
@@ -10,6 +10,8 @@
 
 QVApplication::QVApplication(int &argc, char **argv) : QApplication(argc, argv)
 {
+    setDesktopFileName("com.interversehq.qView.desktop");
+
     // Connections
     connect(&actionManager, &ActionManager::recentsMenuUpdated, this, &QVApplication::recentsMenuUpdated);
     connect(&updateChecker, &UpdateChecker::checkedUpdates, this, &QVApplication::checkedUpdates);
