42a718cd828359c90449ea7158065ff5c499cbfc
Benjamin O <jeep70cp@gmail.com>
2022-01-10 21:38:27 -0500

    Conditionalize qmake apple arch for old macosx build

--- ci/scripts/build.ps1
+++ ci/scripts/build.ps1
@@ -9,7 +9,12 @@ if ($IsWindows) {
     ci/scripts/vcvars.ps1
 }
 
-qmake QMAKE_APPLE_DEVICE_ARCHS="x86_64 arm64" $args[0] PREFIX=$Prefix
+if ((qmake --version -split '\n')[1][17] == '6') {
+    qmake QMAKE_APPLE_DEVICE_ARCHS="x86_64 arm64" $args[0] PREFIX=$Prefix
+} else {
+    qmake $args[0] PREFIX=$Prefix
+}
+
 
 if ($IsWindows) {
     nmake
