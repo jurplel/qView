75e867db30d37c8b11f3321f4af380d91a6f209b
jurplel <jeep70cp@gmail.com>
2022-01-11 17:38:47 -0500

    Fix linux appimage naming

--- ci/scripts/linuxdeployqt.sh
+++ ci/scripts/linuxdeployqt.sh
@@ -24,7 +24,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/
 ../linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/com.interversehq.qView.desktop -appimage -updateinformation="gh-releases-zsync|jurplel|qView|latest|qView-*x86_64.AppImage.zsync" $PLUGINS
 
 if [ $1 != "" ]; then
-    mv *.AppImage qView-nightly-$(buildNumString)-x86_64.AppImage
+    mv *.AppImage qView-nightly-$1-x86_64.AppImage
 else
     mv *.AppImage qView-$VERSION-x86_64.AppImage
 fi
