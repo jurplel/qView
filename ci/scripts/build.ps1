44c974d2874a0299534aca146b80df0ff9abfe48
Benjamin O <jeep70cp@gmail.com>
2022-01-10 21:51:05 -0500

    Whoops i meant eq

--- ci/scripts/build.ps1
+++ ci/scripts/build.ps1
@@ -9,7 +9,7 @@ if ($IsWindows) {
     ci/scripts/vcvars.ps1
 }
 
-if ((qmake --version -split '\n')[1][17] == '6') {
+if ((qmake --version -split '\n')[1][17] -eq '6') {
     qmake QMAKE_APPLE_DEVICE_ARCHS="x86_64 arm64" $args[0] PREFIX=$Prefix
 } else {
     qmake $args[0] PREFIX=$Prefix
