a96ba95fc6eb86c07a2b76ad13f038b82753c78d
J.D. Purcell <jdpurcell@gmail.com>
2022-12-16 17:35:26 -0500

    Fix nightly build number

--- ci/scripts/build.ps1
+++ ci/scripts/build.ps1
@@ -10,9 +10,9 @@ if ($IsWindows) {
 }
 
 if ((qmake --version -split '\n')[1][17] -eq '6') {
-    qmake QMAKE_APPLE_DEVICE_ARCHS="x86_64 arm64" $args[0] PREFIX=$Prefix DEFINES+="$env:NIGHTLYDEFINES"
+    qmake QMAKE_APPLE_DEVICE_ARCHS="x86_64 arm64" $args[0] PREFIX=$Prefix DEFINES+="$env:nightlyDefines"
 } else {
-    qmake $args[0] PREFIX=$Prefix DEFINES+="$env:NIGHTLYDEFINES"
+    qmake $args[0] PREFIX=$Prefix DEFINES+="$env:nightlyDefines"
 }
 
 
