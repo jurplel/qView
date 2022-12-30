7b4d0d8c9fd985bc0f418033d325be6334420427
J.D. Purcell <jdpurcell@gmail.com>
2022-12-29 21:37:45 -0500

    Fix "Recent" list getting randomly reset

--- src/actionmanager.cpp
+++ src/actionmanager.cpp
@@ -9,8 +9,13 @@
 
 ActionManager::ActionManager(QObject *parent) : QObject(parent)
 {
+    isSaveRecentsEnabled = true;
     recentsListMaxLength = 10;
     openWithMaxLength = 10;
+
+    // Connect to settings signal
+    connect(&qvApp->getSettingsManager(), &SettingsManager::settingsUpdated, this, &ActionManager::settingsUpdated);
+    settingsUpdated();
 
     initializeActionLibrary();
 
@@ -25,9 +30,6 @@ ActionManager::ActionManager(QObject *parent) : QObject(parent)
     windowMenu = new QMenu(tr("Window"));
     QVCocoaFunctions::setWindowMenu(windowMenu);
 #endif
-
-    // Connect to settings signal
-    connect(&qvApp->getSettingsManager(), &SettingsManager::settingsUpdated, this, &ActionManager::settingsUpdated);
 }
 
 ActionManager::~ActionManager()
