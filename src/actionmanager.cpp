f420eaaad8eded90cbe0177cde131d66137b970f
Benjamin O <jeep70cp@gmail.com>
2023-01-11 18:51:33 +0000

    Fix recents icon visibility regression on linux

--- src/actionmanager.cpp
+++ src/actionmanager.cpp
@@ -448,8 +448,8 @@ void ActionManager::updateRecentsMenu()
                     icon = getCacheableIcon("filetype:" + fileInfo.suffix(), icon);
 #endif
                 action->setIcon(icon);
-                action->setIconVisibleInMenu(true);
 #endif
+                action->setIconVisibleInMenu(true);
             }
             else
             {
