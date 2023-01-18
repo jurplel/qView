b7d5ddcff80f6184ad07202455fdd41fbbfe8db5
J.D. Purcell <jdpurcell@gmail.com>
2023-01-18 18:13:29 -0500

    Cleanup

--- src/qvgraphicsview.cpp
+++ src/qvgraphicsview.cpp
@@ -651,8 +651,8 @@ qreal QVGraphicsView::getContentToViewportRatio() const
 
 QTransform QVGraphicsView::getTransformWithNoScaling() const
 {
-    qreal currentTransformScale = transform().mapRect(QRectF(QPointF(), QSizeF(1, 1))).width();
-    return QTransform(transform()).scale(1.0 / currentTransformScale, 1.0 / currentTransformScale);
+    QRectF unityRect = transform().mapRect(QRectF(0, 0, 1, 1));
+    return transform().scale(1.0 / unityRect.width(), 1.0 / unityRect.height());
 }
 
 qreal QVGraphicsView::getScaleAdjustment() const
