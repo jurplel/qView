c6ad165b772d5d312a4a805ab43c6a4fd6ff5a16
J.D. Purcell <jdpurcell@gmail.com>
2023-08-21 21:36:05 -0400

    Fix window getting unmaximized/resized when exiting full screen mode.
    This occurred with "window matches image size" set to "when opening images".

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1084,7 +1084,6 @@ void MainWindow::toggleFullScreen()
     if (windowState() == Qt::WindowFullScreen)
     {
         setWindowState(storedWindowState);
-        setWindowSize();
     }
     else
     {
