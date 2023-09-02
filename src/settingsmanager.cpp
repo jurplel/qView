0e6f1987fdcba69b0d030ce1b3c0f3f02fcadb42
J.D. Purcell <jdpurcell@gmail.com>
2023-09-02 17:06:49 -0400

    Change default fit overscan setting to 0.

--- src/settingsmanager.cpp
+++ src/settingsmanager.cpp
@@ -174,7 +174,7 @@ void SettingsManager::initializeSettingsLibrary()
 #endif
     settingsLibrary.insert("cropmode", {0, {}});
     settingsLibrary.insert("pastactualsizeenabled", {true, {}});
-    settingsLibrary.insert("fitoverscan", {1, {}});
+    settingsLibrary.insert("fitoverscan", {0, {}});
     settingsLibrary.insert("constrainimageposition", {true, {}});
     settingsLibrary.insert("constraincentersmallimage", {true, {}});
     settingsLibrary.insert("colorspaceconversion", {1, {}});
